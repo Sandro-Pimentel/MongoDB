3b2bf299fe6d3e17893bbc03cbac8561e52bea1a
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2025-03-25 15:18:41 -0300

    Creating the base connection
    Creating the base connection and a sample document.

---
+++ Python/connection.py
@@ -0,0 +1,14 @@
+import os
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGODB_URI = os.environ["MONGODB_URI"]
+
+client = MongoClient(MONGODB_URI)
+
+for db_name in client.list_database_names():
+    print(db_name)
+
+client.close()
---
+++ sample_doc.py
@@ -0,0 +1,17 @@
+new_document = {
+    "account_holder": "Addison Shelton",
+    "account_id": "MDB955769550",
+    "account_type": "checking",
+    "years_active": 5,
+    "address": {
+        "city": "Ridgewood",
+        "zip": "11385",
+        "street": "Menahan St",
+        "number": "1712",
+    },
+    "transfers_complete": [
+        "TR628105502",
+        "TR197586149",
+        "TR586833243",
+    ],
+}
